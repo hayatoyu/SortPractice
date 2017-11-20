7811cc325b92b06d6519f6f082d084509ddf7a49
hayatoyu <f19870421@gmail.com>
2017-11-20 09:14:33 +0800

    temp2

---
+++ SortPractice/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 8, 10, 2, 7, 5, 9, 6, 78, 56, 34 };
+            string[] b = new string[] { "Alex", "Bob", "Eric", "David", "Tina", "Judy", "Leo", "Mike" };
+
+            #region 氣泡排序法
+            // ========== BubbleSort Test ==========
+            Console.WriteLine("氣泡排序法測試");
+            int[] b_a = new int[11];
+            a.CopyTo(b_a, 0);
+            string[] b_b = new string[8];
+            b.CopyTo(b_b, 0);
+            SortingPractice.BubbleSort(ref b_a);
+            for (int i = 0; i < b_a.Length; i++)
+                Console.Write(b_a[i] + "\t");
+            Console.Write("\n");
+            SortingPractice.BubbleSort(ref b_b);
+            for (int i = 0; i < b_b.Length; i++)
+                Console.Write(b_b[i] + "\t");
+            Console.Write("\n請按任意鍵繼續");
+            Console.ReadLine();
+
+            #endregion
+
+
+            #region 插入排序法
+            // ========== Insertion Test ==========
+            Console.WriteLine("插入排序法測試");
+            int[] i_a = new int[11];
+            a.CopyTo(i_a, 0);
+            string[] i_b = new string[8];
+            b.CopyTo(i_b, 0);
+            SortingPractice.InsertionSort(ref i_a);
+            for (int i = 0; i < i_a.Length; i++)
+                Console.Write(i_a[i] + "\t");
+            Console.Write("\n");
+            SortingPractice.InsertionSort(ref i_b);
+            for (int i = 0; i < i_b.Length; i++)
+                Console.Write(i_b[i] + "\t");
+            Console.Write("\n請按任意鍵繼續");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ SortPractice/SortingPractice.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortPractice
+{
+    class SortingPractice
+    {
+        public static void BubbleSort<T> (ref T[] array) where T : IComparable<T>
+        {
+            T temp;
+            bool swap;
+            for(int i = 0;i < array.Length;i++)
+            {
+                swap = false;
+                for(int j = 0;j < array.Length - i - 1;j++)
+                {
+                    if(array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        if (!swap)
+                            swap = true;
+                    }
+                }
+                if (!swap)
+                    return;
+            }
+
+        }
+
+        public static void InsertionSort<T> (ref T[] array) where T : IComparable<T>
+        {
+            for(int i = 1;i < array.Length;i++)
+            {
+                T t = array[i];
+                for(int j = i - 1;j >= 0;j--)
+                {                    
+                    if (array[j].CompareTo(t) > 0)
+                    {
+                        array[j + 1] = array[j];
+                        array[j] = t;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            
+        }
+
+        public static void MergeSort<T> (ref T[] array) where T : IComparable<T>
+        {
+            array = Divide(array.ToList()).ToArray();            
+        }
+
+        private static List<T> Divide<T> (List<T> list) where T : IComparable<T>
+        {
+            if (list.Count <= 1)
+                return list;
+            int mid = list.Count / 2;
+
+            var left = list.GetRange(0, mid);
+            var right = list.GetRange(mid, list.Count - mid);
+
+            left = Divide(left);
+            right = Divide(right);
+
+            return Merge(left, right);
+            
+        }
+        private static List<T> Merge<T> (List<T> left,List<T> right) where T : IComparable<T>
+        {
+            List<T> temp = new List<T>();
+            while(left.Count > 0 && right.Count > 0)
+            {
+                if(left[0].CompareTo(right[0]) > 0)
+                {
+                    temp.Add(right[0]);
+                    right.RemoveAt(0);
+                }
+                else
+                {
+                    temp.Add(left[0]);
+                    left.RemoveAt(0);
+                }
+            }
+            if(left.Count > 0)
+            {
+                
+            }
+        }
+    }
+}
